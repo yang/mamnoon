b5b352b72247842c378e898e64ba8d59cf3509ce
joe waine <joewaine@wawa1114s-imac.local>
2020-06-23 16:43:59 -0700

    f

--- src/components/Coverslide.vue
+++ src/components/Coverslide.vue
@@ -1,7 +1,46 @@
 <template>
 <div class="cover">
-    <a href="http://hotmail.com" target="_blank">hotmail</a>
-        <button>1234</button>
-        124456<img  src="demo/attic.jpg"/>
+    <div class="header">
+June 23
+    </div>
+
+        <img  src="demo/attic.jpg"/>
+    <div class="bottom-button">
+              <div id="mama-dummy-button" class="TockButton-buttonContainer" style="cursor: pointer;">
+                <div data-tock-reserve="true" class="TockButton-link">
+                  <div class="TockButton TockButton-blue">
+                    <span class="TockWidget-B2"><OrderStar /></span>
+                  </div>
+                </div>
+              </div>
+    </div>
     </div>
 </template>
+
+
+
+
+<script>  
+
+import OrderStar from '@/components/svgIcons/OrderStar.vue'
+export default{
+name: 'coverslide',
+components: {
+    OrderStar
+}
+}
+
+</script>
+
+<style lang="scss">
+.header,
+.bottom-button{
+width: 100%;
+height: 100px;
+background: red;
+color: yellow;
+text-align: center;
+}
+
+
+</style>
