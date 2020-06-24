391a9ba724f78145e12c801e6fc1ec02e6ad2231
joe waine <joewaine@wawa1114s-imac.local>
2020-06-23 17:34:17 -0700

    up index

--- src/components/Coverslide.vue
+++ src/components/Coverslide.vue
@@ -8,7 +8,16 @@ June 23
     <div class="bottom-button">
    
   info
-   
+
+
+
+<div style="background: red">
+    <div class="tock-widget" style="position: absolute;right: 0;top: 50%;transform: translateY(-50%);">
+
+      <div id="Tock_widget_container" data-tock-display-mode="Button" data-tock-color-mode="White" data-tock-locale="en-us" data-tock-timezone="America/Los_Angeles"></div>
+    </div>
+  </div>
+
    
     </div>
     </div>
