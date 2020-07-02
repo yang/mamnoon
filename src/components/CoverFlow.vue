2a068626a8e61e2fbb9f8c68658745d90f46d8a3
joe waine <joe.waine@gmail.com>
2020-07-02 12:20:20 -0700

    cf

--- src/components/CoverFlow.vue
+++ src/components/CoverFlow.vue
@@ -14,11 +14,13 @@
 <!-- {{products.items}} -->
 
 
+title: {{title}}
+description: {{description}}
 
 	<div id="container"></div>
-    <!-- <div style="width:480px;">
+    <div style="width:480px;display:none;">
 		<div style="float:right;">Focused: <b id="focusindex">0</b> | Clicked: <b id="clickindex">0</b></div>
-		<p>
+		<!-- <p>
 			<a href="javascript:;" @click="coverflow().left();">Left</a> - <a href="javascript:;" @click="coverflow().right();">Right</a>
 		</p>
 		<p>
@@ -26,11 +28,14 @@
 		</p>
 		<p>
 			<input id="toval" value="0" size="1"> <a href="javascript:;" @click="coverflow().to(document.getElementById('toval').value);">To</a>
-		</p>
-	</div> -->
+		</p> -->
+	</div>
 	
 	<!-- <button @click="reset()">Reset</button> -->
 	<!-- <button @click="coverflow().remove()">Remove</button> -->
+
+
+
 
 
 
@@ -47,21 +52,42 @@
 
 export default {
 		name: 'coverflow',
+		data(){
+			return{
+			products: this.products.items,
+			title: this.products.items[0].title,
+			description: this.products.items[0].description
+		}
+		},
   mounted(){
-	  console.log(123)
 	  this.reset();
   },
   	props: ['products'],
   methods: {
+	  returnProducts(index) {
+// console.log(this.title[index].title)
+
+this.title = this.products.items[index].title
+this.description = this.products.items[index].description
+
+
+
+
+// this.title = this.title[index]
+	  },
 	  reset () {
+
+
+		let that = this
+
 		  console.log(123);
 		  			coverflow('container').remove();
 			
 			//$('#container').coverflow({
 			coverflow('container').setup({
 				width: '100%',
-				item: 1,
-				rotatedelay: 2000,
+				item: 0,
+				rotatedelay: 0,
 				// mousewheel: false,
 				// mode: 'flash',
 				backgroundcolor: '#F05D5B',
@@ -79,9 +105,19 @@ export default {
 			})
 			.on('ready', function() {
 
-				// this.on('focus', function(index) {
-				// 	document.getElementById('focusindex').innerHTML = index;
-				// });
+
+					// 		document.getElementById('focusindex').innerHTML = index;
+
+
+					// that.returnProducts(index)
+
+				this.on('focus', function(index) {
+					// console.log(this.config.playlist[index])
+					document.getElementById('focusindex').innerHTML = index;
+
+
+					that.returnProducts(index)
+				});
 				
 				// this.on('click', function(index, link) {
 				// 	document.getElementById('clickindex').innerHTML = index;
@@ -96,3 +132,10 @@ export default {
   }
 }
 </script>
+
+
+<style lang="scss">
+.coverflow-text{
+	display: none;
+}
+</style>
