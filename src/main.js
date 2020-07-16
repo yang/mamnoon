b93cb44e6364a74c01c27bf0d826b82c58f22e35
joe waine <joe.waine@gmail.com>
2020-07-16 15:58:32 -0700

    update

--- src/main.js
+++ src/main.js
@@ -16,8 +16,8 @@ import "./sevenRooms2.js"
 // Vue.component('CoverFlowSlide', CoverFlowSlide)
 
 const base = axios.create({
-  baseURL: "http://localhost:4000"
-// baseURL: 'https://young-hamlet-03679.herokuapp.com/'
+  // baseURL: "http://localhost:4000"
+baseURL: 'https://young-hamlet-03679.herokuapp.com/'
 });
 
 
