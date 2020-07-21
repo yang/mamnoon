16418ce0d87b6c80d06f35054e1748d37c4f15eb
joe waine <joe.waine@gmail.com>
2020-07-21 10:29:37 -0700

    eddit base url

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ import "./sevenRooms2.js"
 
 const base = axios.create({
   // baseURL: "http://localhost:4000"
-baseURL: 'https://young-hamlet-03679.herokuapp.com'
+baseURL: 'http://young-hamlet-03679.herokuapp.com'
 });
 
 
