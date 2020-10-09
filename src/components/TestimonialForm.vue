f77ea641b5a7dea5a3b51b70997bb398b5c39261
joe waine <joe.waine@gmail.com>
2020-10-09 11:58:37 -0700

    d

--- src/components/TestimonialForm.vue
+++ src/components/TestimonialForm.vue
@@ -117,7 +117,7 @@ props: ['emailAddress'],
 methods: {
     async checkForm() {
       try {
-        let response = await this.$http.post("http://localhost:4000/user/submittestimonial", this.messageBody);
+        let response = await this.$http.post("https://young-hamlet-03679.herokuapp.com/user/submittestimonial", this.messageBody);
         // console.log(response.data.user.testimonials);
         console.log(response.data)
         this.testimonials = response.data.user.testimonials
@@ -137,7 +137,7 @@ methods: {
 // console.log('get testimonials')
 // console.log(this.emailAddress)
       try {
-        let response = await this.$http.get("http://localhost:4000/user/gettestimonials/" + this.emailAddress);
+        let response = await this.$http.get("https://young-hamlet-03679.herokuapp.com/user/gettestimonials/" + this.emailAddress);
 
 
        console.log(response.data)
