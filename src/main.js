d097a721bd5488660b95d29f37faa87fa15faba0
joe waine <joe.waine@gmail.com>
2020-10-16 15:05:41 -0700

    update

--- src/main.js
+++ src/main.js
@@ -16,8 +16,8 @@ import VueCurrencyInput from 'vue-currency-input'
 Vue.use(VueCurrencyInput)
 
 const base = axios.create({
-    // baseURL: "https://young-hamlet-03679.herokuapp.com"
-    baseURL: "http://localhost:4000"
+    baseURL: "https://young-hamlet-03679.herokuapp.com"
+    // baseURL: "http://localhost:4000"
     }); 
 
 
