70f281e464744da651384e0c530148c29a4eacb4
joe waine <joe.waine@gmail.com>
2020-11-09 13:40:36 -0800

    ideas

--- src/main.js
+++ src/main.js
@@ -18,7 +18,8 @@ Vue.use(VueCurrencyInput)
 
 const base = axios.create({
   // baseURL: process.env.NODE_ENV === "development" ? "http://localhost:4000" : "https://young-hamlet-03679.herokuapp.com"
-  baseURL: "http://localhost:4000"
+  // baseURL: "http://localhost:4000" 
+  baseURL: "https://young-hamlet-03679.herokuapp.com"
 }); 
 
 
