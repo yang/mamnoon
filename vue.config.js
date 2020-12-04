f4f17591dda3ac1cf2383b7d9030d489b734c002
joe waine <joe.waine@gmail.com>
2020-12-04 14:43:52 -0800

    ip

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,12 @@
 const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
-
 module.exports = {
   configureWebpack: {
     plugins: [
       new VuetifyLoaderPlugin()
     ],
+    splitChunks: {
+      minSize: 10000,
+      maxSize: 250000,
+  }
   },
 }
