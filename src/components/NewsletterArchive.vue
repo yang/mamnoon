7cddf1f0a2924ed9c19ef701271205049881f3c3
joe waine <joe.waine@gmail.com>
2021-03-31 10:23:43 -0700

    na

--- src/components/NewsletterArchive.vue
+++ src/components/NewsletterArchive.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="container pad ff">
     <div class="row"><div class="col-md-12 mb20"><h1>Newsletter Archive</h1></div></div>
-    <div class="row"><div class="col-md-12 mb20"><div id="gist"></div></div></div>
+    <div class="row"><div class="col-md-12 mb20"><div id="gist" class="gist"></div></div></div>
 </div>
 </template> 
 <script>
@@ -13,7 +13,21 @@ export default {
 
 
 
-    postscribe('#gist', `<script src="https://mamnoonrestaurant.createsend.com/t/i/p/ethth/0/1/1/1/1/"><\/script>`)
+    postscribe('#gist', `<script src="https://mamnoonrestaurant.createsend.com/t/i/p/ethth/0/1/0/1/1/"><\/script>`)
+
+
+
+
+
+setTimeout(function(){ 
+var elements = document.getElementsByClassName("gist");
+for (var i = 0; i < elements.length; i++) {
+  elements[i].innerHTML = elements[i].innerHTML.replace(/,/g,'');
+  console.log('done')
+}
+  }, 1000);
+
+
   }
 }
 </script>
@@ -23,6 +37,16 @@ export default {
 .pad {
     padding-top: 50px;
     padding-bottom: 50px;
+}
+
+
+#gist {
+  ul{
+    padding-left: 0;
+    li a{
+    display: block;
+    }
+  }
 }
 </style>
 
