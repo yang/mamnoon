be5b177915b31b5914000146540c00729847502d
joe waine <joe.waine@gmail.com>
2021-07-29 15:02:06 -0700

    f

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -149,7 +149,7 @@ export default {
   methods: {
  isMobile() {
    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-     return true
+     return false
    } else {
      return false
    }
