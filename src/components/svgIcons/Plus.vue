7a5ca16248fa74180bd7f436cf00e443537723dd
joe waine <joe.waine@gmail.com>
2021-07-29 17:07:28 -0700

    f

---
+++ src/components/svgIcons/Plus.vue
@@ -0,0 +1,26 @@
+<template>
+
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 24 24" style="enable-background:new 0 0 24 24;" xml:space="preserve">
+<polygon points="24,11 13,11 13,0 11,0 11,11 0,11 0,13 11,13 11,24 13,24 13,13 24,13 "/>
+</svg>
+
+
+
+</template>
+
+
+<script>
+export default{
+
+}
+
+</script>
+
+
+<style>
+</style>
+
+
+
+
