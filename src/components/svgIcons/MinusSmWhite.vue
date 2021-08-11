30dd954a5cb90aa62a68b72b46b4c4dc695d78dc
alexagenesis1992 <alexagenesis1992@gmail.com>
2021-08-11 15:08:03 -0700

    f

---
+++ src/components/svgIcons/MinusSmWhite.vue
@@ -0,0 +1,33 @@
+<template>
+<svg class="minus-sm-white" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 24 24" style="enable-background:new 0 0 24 24;" xml:space="preserve">
+<rect x="11" y="3.4" transform="matrix(6.123234e-17 -1 1 6.123234e-17 0 24)" width="2" height="17.1"/>
+</svg>
+
+
+
+</template>
+
+
+<script>
+export default{
+
+}
+
+</script>
+
+
+<style>
+
+
+.minus-sm-white{
+	margin-top: 3px;
+	width: 50px !important;
+fill: #ffffff;
+
+}
+</style>
+
+
+
+
