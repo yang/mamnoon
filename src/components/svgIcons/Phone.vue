ff021ea0c3486ea673565efb2f97cb053f47dc44
alexagenesis1992 <alexagenesis1992@gmail.com>
2021-11-10 15:16:09 -0800

    f

--- src/components/svgIcons/Phone.vue
+++ src/components/svgIcons/Phone.vue
@@ -1,6 +1,6 @@
 <template>
 
-<svg class="cellPhone" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 viewBox="0 0 48 48" :style="{'enable-background':'new 0 0 48 48', 'width': this.width ? this.width :'20px'}" xml:space="preserve">
 <path :fill="color" d="M43.8,1.7h-4.9C21.6,2,1.8,24.1,1.7,39v4.9c0,1.3,1,2.5,2.5,2.5l9.8-0.1c1.3,0,2.5-1.2,2.5-2.5l0.3-10.8l-6.2-5.2
 	c0-3.4,13.8-17.2,17.3-17.3l5.6,6.2l10.4-0.3c1.3,0,2.5-1.2,2.5-2.5l-0.1-9.6C46.3,2.8,45.2,1.7,43.8,1.7z"/>
