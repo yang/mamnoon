12b9b5280bddf60f222892670dd40e2e523a120b
alexagenesis1992 <alexagenesis1992@gmail.com>
2022-03-08 12:52:51 -0800

    g

--- src/components/UpserveOloTestingNewProcessing.vue
+++ src/components/UpserveOloTestingNewProcessing.vue
@@ -2188,8 +2188,8 @@ cart empty
       </div>
       </div>
       </section>
-      
-<!--<pre v-if="this.title === 'Mamnoon'">{{this.$store.state.storeMamnoon}}</pre>
+      <!--
+<pre v-if="this.title === 'Mamnoon'">{{this.$store.state.storeMamnoon}}</pre>
 <pre v-if="this.title === 'Mamnoon Street'">{{this.$store.state.storeStreet}}</pre>
 <pre v-if="this.title === 'Mbar'">{{this.$store.state.storeMbar}}</pre>-->
    <!--// ccc-->
