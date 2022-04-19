396242be66bbb99abda5b6caef94fe265a7b28d0
ZachSpriggs <60864026+ZachSpriggs@users.noreply.github.com>
2022-04-18 17:27:33 -0700

    update to styles

--- src/components/hanoonCareerModule.vue
+++ src/components/hanoonCareerModule.vue
@@ -14,55 +14,39 @@
       <div class="col-md-4"></div>
       <div class="hanoonJobs">
         <div class="hanoon">
-          <a :href="'www.google.com'">apply here</a>
-          <br />
-          <div class="nowHiring">
-            <span class="title">now hiring for the</span>
-            <br />
-            <span class="title">following positions:</span>
-          </div>
+          
           <div class="nowHiring">
             <span class="listTitle">floor</span>
             <br>
-            <span class="listItem">server</span>
+            <span class="listItem">server - <a href="https://www.indeed.com/jobs?q=mama%20restaurant%20group&l=Seattle%2C%20WA&vjk=5baf193977ab40fd">apply here</a></span>
             <br>
-            <span class="listItem">host</span>
+            <span class="listItem">server assistant - <a href="https://www.indeed.com/jobs?q=mama%20restaurant%20group&l=Seattle%2C%20WA&vjk=aa7669a48616dfc4">apply here</a></span>
             <br>
-            <span class="listItem">busser/runner</span>
+            <span class="listItem">host - <a href="https://www.indeed.com/jobs?q=mama%20restaurant%20group&l=Seattle%2C%20WA&vjk=cf6ee5f8f94a80f2">apply here</a></span>
             <br>
-            <span class="listItem">dishwasher</span>
+            <span class="listItem">dishwasher - <a href="https://www.indeed.com/jobs?q=mama%20restaurant%20group&l=Seattle%2C%20WA&vjk=8ab8b42f6599770d">apply here</a></span>
           </div>
           <div class="nowHiring">
             <span class="listTitle">bar</span>
             <br>
-            <span class="listItem">barback</span>
-            <br>
-            <span class="listItem">bartender</span>
-            <br>
-            <span class="listItem">bar lead</span>
+            <span class="listItem">bartender - <a href="https://www.indeed.com/jobs?q=mama%20restaurant%20group&l=Seattle%2C%20WA&vjk=a55d9fa2e84f96ff">apply here</a></span>
             <br>
           </div>
           <div class="nowHiring">
             <span class="listTitle">management</span>
             <br>
-            <span class="listItem">floor manager</span>
-            <br>
-            <span class="listItem">wine/floor manager</span>
-            <br>
-            <span class="listItem">sous chef</span>
-            <br>
-            <span class="listItem">chef-de-cuiside</span>
-            <br>
-            <span class="listItem">assistant general manager</span>
+            <span class="listItem">sous chef - <a href="https://www.indeed.com/jobs?q=mama%20restaurant%20group&l=Seattle%2C%20WA&vjk=7a7fe4a9e6b47af0">apply here</a></span>
             <br>
             <span class="listItem">general manager - <a href="https://www.indeed.com/job/general-manager-new-restaurant-opening-7bcfa509dd53432b">apply here</a></span>
+                        <br>
+            <span class="listItem">front of house manager - <a href="https://www.indeed.com/jobs?q=mama%20restaurant%20group&l=Seattle%2C%20WA&vjk=817b3f3f558a7fe1">apply here</a></span>
           </div>
           <div class="nowHiring">
             <span class="listTitle">kitchen</span>
             <br>
-            <span class="listItem">prep cook</span>
+            <span class="listItem">prep cook - <a href="https://www.indeed.com/jobs?q=mama%20restaurant%20group&l=Seattle%2C%20WA&vjk=2a527043db991e0d">apply here</a></span>
             <br>
-            <span class="listItem">line cook</span>
+            <span class="listItem">line cook - <a href="https://www.indeed.com/jobs?q=mama%20restaurant%20group&l=Seattle%2C%20WA&vjk=f4b0c1110191b420">apply here</a></span>
           </div>
           <br>
           <br>
